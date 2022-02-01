3bdc3d14678c2c251371a8ce58cc59614310c55f
Anish Roy <6275anishroy@gmail.com>
2022-02-01 22:29:30 +0530

    dynamic classes

--- src/components/timetable/index.js
+++ src/components/timetable/index.js
@@ -1,5 +1,57 @@
 import React from 'react';
 import './styles.scss';
+import randomColor from 'randomcolor';
+
+const schedule = [
+    {
+        label: "Machine Learning Grp 1",
+        from: 1030,
+        to: 1230,
+        day: 1
+    },
+    {
+        label: 'Blockchain Grp 1',
+        from: 800,
+        to: 1000,
+        day: 2
+    },
+    {
+        label: 'Basic Psychology Grp 1',
+        from: 1330,
+        to: 1530,
+        day: 1
+    },
+    {
+        label: 'Introduction to Java Enterprise Framework',
+        from: 1130,
+        to: 1330,
+        day: 2
+    },
+    {
+        label: 'Server Side Web Technology',
+        from: 1300,
+        to: 1530,
+        day: 4
+    },
+    {
+        label: 'Internet of Things Grp 1',
+        from: 1600,
+        to: 1800,
+        day: 2
+    },
+    {
+        label: 'Internet of Things - Grp 1',
+        from: 1630,
+        to: 1830,
+        day: 4
+    },
+    {
+        label: 'Current Trends and Practices in IT',
+        day: 5,
+        from: 1000,
+        to: 1200
+    }
+];
 
 function Top() {
     return (
@@ -13,6 +65,20 @@ function Top() {
 
     )
 }
+
+function getColCount(time) {
+    let ti = true, i = 1, t = 0;
+    while (t < time) {
+        ti = !ti
+        t += 30;
+        if (ti) {
+            t += 40;
+        }
+        i++;
+    }
+    return i;
+}
+
 export default function TimeTable() {
     const times = ["00"];
     for (let t = 30, ti = true; t <= 2400; t += 30, ti = !ti) {
@@ -50,10 +116,23 @@ export default function TimeTable() {
                         )}
                     </div>
                     <div className='classes'>
-                        <div className='class class1'>Machine Learning Grp 1</div>
-                        <div className='class class2'>Blockchain Grp 1</div>
-                        <div className='class class3'>Server Side Web Technology</div>
-                        <div className='class class4'>IOT Grp 1</div>
+                        {schedule.map(_class => (
+                            <div className='class'
+                                style={{
+                                    gridRow: _class.day,
+                                    background: randomColor({
+                                        seed: _class.label,
+                                        luminosity: 'bright'
+                                    }),
+                                    boxShadow: `3px 3px 9px ${randomColor({
+                                        seed: _class.label,
+                                        luminosity: 'bright',
+                                        format: 'hex'
+                                    })}99`,
+                                    gridColumnStart: getColCount(_class.from),
+                                    gridColumnEnd: getColCount(_class.to)
+                                }}>{_class.label}</div>
+                        ))}
                     </div>
                 </div>
             </div>
