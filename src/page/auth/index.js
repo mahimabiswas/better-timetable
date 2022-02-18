63680c1bec17ef7b33a9379e5d6d5d3e0d430303
Anish Roy <6275anishroy@gmail.com>
2022-02-18 17:03:30 +0530

    .

--- src/page/auth/index.js
+++ src/page/auth/index.js
@@ -1,12 +1,10 @@
-import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './styles.scss';
 import logo from 'assets/logo.svg';
 import appSs from 'assets/app-light.png';
 import SignIn from './signin';
 import SignUp from './signup';
 export default function Auth() {
-    let { path } = useRouteMatch();
-
     return (
         <div className="auth">
             <div className='container'>
@@ -23,7 +21,6 @@ export default function Auth() {
                     <Route path="/auth/login" children={<SignIn />} />
                 </Switch>
             </div>
-            {/* background-image: linear-gradient(to left bottom, #000000, #0d0d0d, #161616, #1d1d1d, #252525, #282828, #2a2a2a, #2d2d2d, #2b2b2b, #2a2a2a, #282828, #272727); */}
         </div>
     )
 }
