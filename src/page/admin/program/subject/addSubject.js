bc03a7dfc911b482e95cabfe15f8084ff2b769f7
Anish Roy <6275anishroy@gmail.com>
2022-04-06 16:08:15 +0530

    t

--- src/page/admin/program/subject/addSubject.js
+++ src/page/admin/program/subject/addSubject.js
@@ -1,10 +1,26 @@
 import Modal from 'react-modal';
 import { MdClose } from 'react-icons/md';
 import TextField from 'shared/textfield';
+import { useState } from 'react';
 import Radio from 'shared/radio';
 import Button from 'shared/button';
 
 export default function AddSubject({ open, setOpen }) {
+    const [shortName, setShortName] = useState("");
+    const [longName, setLongName] = useState("");
+    const [subjectType, setSubjectType] = useState(0);
+
+
+    function submit() {
+        const payload = {
+            shortName,
+            longName,
+            subjectType
+        }
+
+        console.log(payload)
+    }
+
     return (
         <Modal
             isOpen={open}
@@ -25,20 +41,20 @@ export default function AddSubject({ open, setOpen }) {
                     border: 'none',
                 }
             }}
-            contentLabel="Example Modal"
+            contentLabel="Add Subject Modal"
         >
             <div className='header'>
                 <h3>Add Subject</h3>
                 <div className='close' onClick={() => { setOpen(false) }}><MdClose /></div>
             </div>
-            <div className='form' style={{ display: 'unset' }}>
-                <TextField title='short name' />
-                <TextField title='long name' styles={{ margin: '12px 0' }} />
+            <div className='form' style={{ display: 'unset', margin: '0 16px' }}>
+                <TextField title='short name' type='text' name="short_name" value={shortName} onChange={setShortName} />
+                <TextField title='long name' type='text' name="long_name" value={longName} onChange={setLongName} styles={{ margin: '12px 0' }} />
                 <Radio title='subject type'
                     options={[{ value: '1', label: 'core' }, { value: '2', label: 'elective' }]}
-                    name="subject-type" defaultSelected={0} onChange={(e) => { console.log(e) }}
+                    name="subject-type" defaultSelected={subjectType} onChange={(e) => { setSubjectType(e) }}
                 />
-                <Button label='Confirm' size='medium' styles={{
+                <Button label='Confirm' size='medium' onClick={submit} styles={{
                     marginTop: '24px'
                 }} />
             </div>
